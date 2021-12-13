c0504416948ca6ae5f19ef336a5f99957b7bcc11
Lucas Paschke <74216653+lpaschke16@users.noreply.github.com>
2021-12-13 15:50:56 -0500

    Update js.js

--- script.js/js.js
+++ script.js/js.js
@@ -18,7 +18,7 @@ function updateOptions() {
 const textAdventure = [
   {
     id: 1,
-    text: 'This is the adventure of Juan, who got lost in the forest when looking for a bird to photograph. Juan needs your help to find his way out. Choose one of the options below',
+    text: 'This is the adventure of Al, who got lost in the sewer system when chasing a mouse down the drain hole. Al needs your help to find his way out. Choose one of the options below',
     options: ["Head south and find the river" , "Go back to the bottom of the hill" ]
   },
 ]

9c771b0e2484db140b6c50b0ceee5f2e6e9a1752
Lucas Paschke <74216653+lpaschke16@users.noreply.github.com>
2021-12-13 15:53:59 -0500

    Update js.js

--- script.js/js.js
+++ script.js/js.js
@@ -19,7 +19,7 @@ const textAdventure = [
   {
     id: 1,
     text: 'This is the adventure of Al, who got lost in the sewer system when chasing a mouse down the drain hole. Al needs your help to find his way out. Choose one of the options below',
-    options: ["Head south and find the river" , "Go back to the bottom of the hill" ]
+    options: ["Head down the left entry-way" , "Go back to flowing water bank" ]
   },
 ]
 

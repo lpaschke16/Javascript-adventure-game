00658cbe6d08ecbf90333caa39bec7958fa45a72
Lucas Paschke <74216653+lpaschke16@users.noreply.github.com>
2021-12-14 14:17:17 -0500

    fixed script

---
+++ script/script.js
@@ -0,0 +1,27 @@
+console.log("script is loading");
+
+let adventureDescriptionText = document.querySelector(".adventure-description");
+let optionButtons = document.getElementsByClassName("button");
+
+function updateAdventureText() {
+  adventureDescriptionText.innerText = textAdventure[0].text;
+}
+
+function updateOptions() {
+  let i = 0;
+  for(i; i < optionButtons.length; i++) {
+    console.log(optionButtons[i]);
+    optionButtons[i].innerText = textAdventure[0].options[i];
+  }
+}
+
+const textAdventure = [
+  {
+    id: 1,
+    text: 'This is the adventure of Al, who got lost in the sewer system when chasing a mouse down the drain hole. Al needs your help to find his way out. Choose one of the options below',
+    options: ["Head down the left entry-way" , "Go back to flowing water bank" ]
+  },
+]
+
+updateAdventureText();
+updateOptions();

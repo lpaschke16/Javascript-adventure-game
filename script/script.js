2fd318070259302f07307b5a15fa94371564b824
Lucas Paschke <74216653+lpaschke16@users.noreply.github.com>
2021-12-14 20:53:42 -0500

    Added 12/14 updates

--- script/script.js
+++ script/script.js
@@ -1,27 +1,69 @@
 console.log("script is loading");
 
-let adventureDescriptionText = document.querySelector(".adventure-description");
-let optionButtons = document.getElementsByClassName("button");
+// select HTML elements
+const descriptionText = document.querySelector(".adventure-description");
+const optionButtons = document.getElementsByClassName("button");
 
-function updateAdventureText() {
-  adventureDescriptionText.innerText = textAdventure[0].text;
+console.log(descriptionText, optionButtons);
+
+// function that starts the game
+function startAdventure() {
+  console.log("Adventure has begun!");
+  showAdventureDescription(2);
 }
 
-function updateOptions() {
-  let i = 0;
-  for(i; i < optionButtons.length; i++) {
-    console.log(optionButtons[i]);
-    optionButtons[i].innerText = textAdventure[0].options[i];
+// function that adds the options and tracks the choices
+
+// create a function that shows the description
+function showAdventureOptions(textAdventureID) {
+  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID );
+  descriptionText.innerText = newDescriptionText.description;
+  //iterate through the buttons and insert the option text in each button
+  for(let i = 0; i < newDescriptionText.options.length; i++) {
+    //console.log(newDescriptionText.options[i].buttontext);
+    optionButtons.innerText = newDescriptionText.options[i].buttontext;
   }
 }
 
+// function that tracks options and passes the id to the showAdventureOptions function
+
+// optional: function that shows end of the game
+
+
+
+// create an object that holds the text for the game and options. It also tracks state
 const textAdventure = [
   {
     id: 1,
     text: 'This is the adventure of Al, who got lost in the sewer system when chasing a mouse down the drain hole. Al needs your help to find his way out. Choose one of the options below',
-    options: ["Head down the left entry-way" , "Go back to flowing water bank" ]
+    options: [
+      {
+        buttontext: "Head down the left entry-way", 
+        setState: {headback: true},
+        nexttext: 2
+      },
+      {
+        buttontext: "Head down the right entry-way",
+        nextText: 2
+      }
+    ]
   },
-]
+  {
+    id: 2,
+    description: "Great! Al can start to see the light outside, but he can see something in the water. What should he do?",
+    options: [
+      {
+        buttontext: "Continue towards the light",
+        setState: {headback: true},
+        nextText: 3
+      },
+      {
+        buttontext: "Jump in the water to face the mystery in the water",
+        nextText: 3
+      }
+    ]
+  },
+  ]
 
-updateAdventureText();
-updateOptions();
+
+textAdventure()
